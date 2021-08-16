405575b78e3259418befb36123c61742ad103636
gildurao <43495596+gildurao@users.noreply.github.com>
2021-08-16 20:36:26 +0000

    Deploying to gh-pages from master @ d9524609af81d76a615e9b1fd638406e2aed836c 🚀

--- fonts.js
+++ fonts.js
@@ -171,7 +171,7 @@ Module['FS_createPath']("/resources", "fonts", true, true);
     }
   
    }
-   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}], "remote_package_size": 145348, "package_uuid": "760fe876-cda4-40e9-b4a7-f51c881ddfda"});
+   loadPackage({"files": [{"filename": "/resources/fonts/Roboto-Regular.ttf", "start": 0, "end": 145348, "audio": 0}], "remote_package_size": 145348, "package_uuid": "57f48944-7b01-4b27-b82e-7ed021a348ef"});
   
   })();
   
